55064c1b7c158adec6c8704afd2c0dd7467eeb3f
pullitdown <595259789@qq.com>
2021-10-19 08:48:33 +0800

    jkl

--- main.cpp
+++ main.cpp
@@ -3,13 +3,20 @@
 using namespace std;
 int main()
 {
-    int a=1;
+    int ad=1;
     int b=2;
-    int k=123.212;
-    int c=a+b;
+    float k=123.212;
+    int arr[10]={1,2,3,45,64,54,65,45};
+    arr[1]=123;
+    int c=ad+b;
     char s='1';
+    printf("%d",&ad);
     cout<<"djdkdj"<<endl;
     cout<<c<<endl;
     char a[6]="1df\" ";
+    char kk=250;
+    int p=kk;/*12ahj&&^^*/
+    cout<<p<<endl;//45678372892^&^&^&^///
+    system("pause");
     return 0;
 }
---
+++ pyc++.cpp
@@ -0,0 +1,11 @@
+#include<iostream.h>
+using namespace std;
+int main(){
+int k=122;
+short a=1;
+long b=2;
+int c=a+b;
+float oo=1.2323;
+cout<<c<<endl;
+return 0;
+}
